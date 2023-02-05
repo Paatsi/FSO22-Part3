f52eb85dea575b1e8c39b94dbe08b1e838590822
Paatsi <paatsidev@gmail.com>
2023-02-05 16:17:21 +0200

    exercise 3.8

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', (req) => { return JSON.stringify(req.body) })
+
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 let persons = [
@@ -16,19 +19,16 @@ let persons = [
       id: 2,
       name: "Ada Lovelace", 
       number: "39-44-5323523"
-      
     },
     { 
       id: 3,
       name: "Dan Abramov", 
       number: "12-43-234345"
-      
     },
     { 
       id: 4,
       name: "Mary Poppendieck", 
       number: "39-23-6423122"
-      
     }
   ]
 
